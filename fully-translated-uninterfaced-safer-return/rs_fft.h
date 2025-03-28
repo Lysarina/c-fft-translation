09e0142998539278b481c7a8c502e4d70d9557ae
Ella Viirola <ella.a.viirola@ericsson.com>
2025-03-28 15:40:23 +0100

    bfefjsel

---
+++ fully-translated-uninterfaced-safer-return/rs_fft.h
@@ -0,0 +1,21 @@
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+
+typedef struct complex_t {
+  double re;
+  double im;
+} complex_t;
+
+typedef struct complex_t complex;
+
+complex *DFT_naive(complex *x, int32_t N);
+
+complex *FFT_CooleyTukey(complex *input, int32_t N, int32_t N1, int32_t N2);
+
+complex *FFT_GoodThomas(complex *input, int32_t N, int32_t N1, int32_t N2);
+
+extern void *malloc(uint64_t);
